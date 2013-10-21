1815e10f498f387ea762f80a3dd6fd9cbc2cfcdf
Adam Brown <adamwbrown@gmail.com>
2013-10-20 19:35:11 -0700

    Fully fleshed out About screen

--- RingMyPhone/src/main/java/com/darkrockstudios/apps/ringmyphone/AboutFragment.java
+++ RingMyPhone/src/main/java/com/darkrockstudios/apps/ringmyphone/AboutFragment.java
@@ -2,22 +2,22 @@ package com.darkrockstudios.apps.ringmyphone;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.URLSpan;
-import android.text.util.Linkify;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * Created by adam on 10/20/13.
  */
-public class AboutFragment extends DialogFragment
+public class AboutFragment extends DialogFragment implements View.OnClickListener
 {
 	public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
 	{
@@ -29,6 +29,34 @@ public class AboutFragment extends DialogFragment
 			dialog.setTitle( R.string.about_title );
 		}
 
+		Button marketButton = (Button) view.findViewById( R.id.ABOUT_market_button );
+		marketButton.setOnClickListener( this );
+
+		MovementMethod linkMovementMethod = LinkMovementMethod.getInstance();
+
+		TextView githubAndroidView = (TextView) view.findViewById( R.id.ABOUT_github_android );
+		githubAndroidView.setMovementMethod( linkMovementMethod );
+		githubAndroidView.setText( getText( R.string.about_body_github_android ) );
+
+		TextView githubPebbleView = (TextView) view.findViewById( R.id.ABOUT_github_pebble );
+		githubPebbleView.setMovementMethod( linkMovementMethod );
+		githubPebbleView.setText( getText( R.string.about_body_github_pebble ) );
+
+		TextView githubFeedbackView = (TextView) view.findViewById( R.id.ABOUT_feedback );
+		githubFeedbackView.setMovementMethod( linkMovementMethod);
+		githubFeedbackView.setText( getText( R.string.about_body_feedback ) );
+
 		return view;
+	}
+
+	@Override
+	public void onClick( View v )
+	{
+		if( v.getId() == R.id.ABOUT_market_button && isAdded() )
+		{
+			Intent intent = new Intent( Intent.ACTION_VIEW );
+			intent.setData( Uri.parse( "market://search?q=pub:Dark+Rock+Studios" ) );
+			startActivity( intent );
+		}
 	}
 }
